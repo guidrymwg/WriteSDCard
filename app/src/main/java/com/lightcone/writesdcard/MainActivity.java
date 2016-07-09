de8954bbce37c86679d53b277966eeeb344d618b
guidrymwg <guidry@utk.edu>
2016-07-08 21:03:56 -0400

    File output working .  cleaning it up.

--- app/src/main/java/com/lightcone/writesdcard/MainActivity.java
+++ app/src/main/java/com/lightcone/writesdcard/MainActivity.java
@@ -1,6 +1,5 @@
 package com.lightcone.writesdcard;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -55,62 +54,81 @@ public class MainActivity extends AppCompatActivity {
             // Can't read or write
             mExternalStorageAvailable = mExternalStorageWriteable = false;
         }
-        tv.append("\n\nExternal Media: readable="
+        tv.append("\n\nEXTERNAL MEDIA: readable="
                 + mExternalStorageAvailable + " writable=" + mExternalStorageWriteable);
     }
 
     /**
-     * Method to write ascii text characters to file on SD card. Note that you must add a
-     * WRITE_EXTERNAL_STORAGE permission to the manifest file or this method will throw
-     * a FileNotFound Exception because you won't have write permission.
+     * Method to write ascii text characters to file on SD card. In earlier versions of Android a
+     * WRITE_EXTERNAL_STORAGE permission must be added to the manifest file or this method will throw
+     * a FileNotFound Exception because you won't have write permission. But not true after
+     * API 18 for files in storage area of app (then no write permission required).
      */
 
     private void writeToSDFile() {
 
-        /* First find the root of the external storage. See
+        // Root of the external file system
+
+        File root0 = android.os.Environment.getExternalStorageDirectory();
+
+        /* Now find the root of the external storage for this app (where the app can place
+        * persistent files that it owns internal to the application and not typically visible
+        * to the user as media).  See
+        *
         *    http://developer.android.com/guide/topics/data/data-storage.html#filesExternal
+        *
         * The method getExternalStorageDirectory(string) returns the user storage associated with the
         * app, which doesn't require write permissions after API 18.  The string argument specifies various
         * regions of this storage (it can be null).  For example,
         *
-        * null specifies the root of the storage for this app
-        * Environment.DIRECTORY_NOTIFICATIONS specifies the Notifications directory of app storage
-        * Environvment.DIRECTORY_DOWNLOADS specifies standard directory for files downloaded by user
-        * Environment.DIRECTORY_PICTURES specifies standard directory for pictures available to user
+        * - null specifies the root of the storage for this app
+        * - Environment.DIRECTORY_NOTIFICATIONS specifies the Notifications directory of app storage
+        * - Environvment.DIRECTORY_DOWNLOADS specifies standard directory for files downloaded by user
+        * - Environment.DIRECTORY_PICTURES specifies standard directory for pictures available to the user
+        * - Environment.DIRECTORY_DOCUMENTS specifies standard directory for documents produced by user
         * etc.
         *
-        * See the constants of the Environment class at
+        * See the fields of the Environment class at
         *    https://developer.android.com/reference/android/os/Environment.html
-        * for other possibilities.
+        * for other possibilities.  For example, on my phone (running Android 6.0.1 in 2016) the root of
+        * the user storage for this specific app is found at
+        *
+        *    /storage/emulated/0/Android/data/com.lightcone.writesdcard/files
         * */
 
-        // File root = android.os.Environment.getExternalStorageDirectory();  // Old version
-        //File root = this.getExternalFilesDir(Environment.DIRECTORY_NOTIFICATIONS);
-        File root = this.getExternalFilesDir(null);
-        tv.append("\nExternal file system root: " + root);
+        // Root of the data directories specific to this app, for which no write
+        // permission is required for Android 4.4 and later.
 
+        File root = this.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+
+        tv.append("\n\nEXTERNAL FILE SYSTEM ROOT DIRECTORY:\n" + root0);
+
+        tv.append("\n\nEXTERNAL APP DATA ROOT DIRECTORY:\n" + root);
+
+        // Create a Documents/download subdirectory in the data area for this app
         // See http://stackoverflow.com/questions/3551821/android-write-to-sd-card-folder
 
         File dir = new File(root.getAbsolutePath() + "/download");
         dir.mkdirs();
         File file = new File(dir, "myData.txt");
 
+        // Must catch FileNotFoundException and IOException
         try {
             FileOutputStream f = new FileOutputStream(file);
             PrintWriter pw = new PrintWriter(f);
-            pw.println("Howdy do to you.");
-            pw.println("Here is a second line.");
+            pw.println("Howdy do to you,");
+            pw.println("and the horse you rode in on.");
             pw.flush();
             pw.close();
             f.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Log.i(TAG, "File not found. Did you" +
-                    " add a WRITE_EXTERNAL_STORAGE permission to the manifest?");
+            Log.i(TAG, "File not found");
         } catch (IOException e) {
             e.printStackTrace();
+            Log.i(TAG, "I/O exception");
         }
-        tv.append("\n\nFile written to:\n" + file);
+        tv.append("\n\nFILE WRITTEN TO:\n" + file);
     }
 
     /**
@@ -119,14 +137,14 @@ public class MainActivity extends AppCompatActivity {
      */
 
     private void readRaw() {
-        tv.append("\n\nData read from res/raw/textfile.txt:\n");
+        tv.append("\n\nDATA READ FROM res/raw/textfile.txt:\n");
         InputStream is = this.getResources().openRawResource(R.raw.textfile);
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr, 8192);    // 2nd arg is buffer size
 
         // More efficient (less readable) implementation of above is the composite expression
-               /*BufferedReader br = new BufferedReader(new InputStreamReader(
-                        this.getResources().openRawResource(R.raw.textfile)), 8192);*/
+        //    BufferedReader br = new BufferedReader(new InputStreamReader(
+        //             this.getResources().openRawResource(R.raw.textfile)), 8192);
 
         try {
             String test;
@@ -142,6 +160,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        tv.append("\n\nThat is all");
+        tv.append("\n\nTHAT IS ALL");
     }
 }
